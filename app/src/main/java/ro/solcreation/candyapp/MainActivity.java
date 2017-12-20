200c38c4cd3a95e560c4bc6e242a07746cf9e801
George Codat Tudoran <office@solcreation.ro>
2017-12-20 09:35:57 +0200

    Adding click event to the list

--- app/src/main/java/ro/solcreation/candyapp/MainActivity.java
+++ app/src/main/java/ro/solcreation/candyapp/MainActivity.java
@@ -3,6 +3,8 @@ package ro.solcreation.candyapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -53,7 +55,15 @@ public class MainActivity extends AppCompatActivity {
         String text = "Hello toast!";
         int duration = Toast.LENGTH_SHORT;
 
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast toast = Toast.makeText(MainActivity.this, ""+i, Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        });
+
     }
 }
